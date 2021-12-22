77dca633019888341e15eeb4e7dcaa249e63d669
marcinzygan <soul@o2.pl>
2021-12-22 22:01:25 +0000

    Hero Section + Media

---
+++ README.md
@@ -0,0 +1,3 @@
+This is my project for Portfolio Website . Started in Dec 2021
+20.12.2021 : I've designed the page in Figma 
+21-22.12.2021 : Coding Navbar + Hero section and Media Query .
---
+++ script.js
@@ -0,0 +1,27 @@
+// NAVBAR
+
+const navToggle  = document.querySelector('.nav-toggle'); 
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle ('nav-open');  
+});
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
+    })
+})
+
+
+// BACK TO HOME ARROW ICON
+
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", function(){
+    const scrollHeight = window.pageYOffset;
+    if( scrollHeight > 500){
+        topLink.classList.add("show-link");   
+    }else{
+        topLink.classList.remove("show-link");
+    }
+});
